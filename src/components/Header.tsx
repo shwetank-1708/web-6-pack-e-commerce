e965b990bf31427474393f858541042177175a36
shwetank-1708 <shwetank.chauhan17@gmail.com>
2024-12-18 20:44:44 +0530

    Added Few Other Changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
-import { FaSearch, FaShoppingBag } from "react-icons/fa";
+import { useState } from "react";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 
+const user = { _id: "aaa", role: "admin" };
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <nav>
+    <nav className="header">
       <Link to={"/"}>Home</Link>
       <Link to={"/search"}>
         {" "}
@@ -13,6 +24,29 @@ const Header = () => {
         {" "}
         <FaShoppingBag />
       </Link>
+
+      {user?._id ? (
+        <>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user.role === "admin" && (
+                <Link to="/admin/dashboard">Admin</Link>
+              )}
+              <Link to="/orders">Orders</Link>
+              <button>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
     </nav>
   );
 };
